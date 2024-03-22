2c83750ca8b8f1f7ddb1a6c4d2a1511f2abcdc7f
Nishant Argade <nishantargade4579@gmail.com>
2024-03-22 23:07:03 +0530

    update readme.md

--- server/app.js
+++ server/app.js
@@ -12,12 +12,10 @@ dbConnection();
 app.use(express.json());
 app.use(cookieParser());
 const corsConfig = {
-  origin: "",
+  origin: process.env.CLIENT_URL,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 app.use(cors(corsConfig));
-app.options("", cors(corsConfig));
 
 app.use("/auth", userRoute);
 
